b21644f5ac83275004df98e495ca90a5dc44c19e
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-10-16 18:45:12 +0600

    add text colors

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -8,7 +8,7 @@ const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
     const [isAdmin] = useAdmin(user?.email)
     return (
-        <div className='flex'>
+        <div className=''>
             <Navbar></Navbar>
             <div className="drawer drawer-mobile">
                 <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
@@ -21,9 +21,10 @@ const DashboardLayout = () => {
                         <li><Link to="/dashboard">My Appointments</Link></li>
                         {
                             isAdmin && <>
-                                <li><Link to="/dashboard/allusers" className='text-yellow-600'>All users</Link></li>
-                                <li><Link to="/dashboard/adddoctor" className='text-yellow-600'>add a doctors </Link></li>
-                                <li><Link to="/dashboard/managedoctor" className='text-yellow-600'>Managedoctor  </Link></li>
+                           
+                                <li><Link to="/dashboard/allusers" className='text-yellow-600 fund-bold text-xl'>All users</Link></li>
+                                <li><Link to="/dashboard/adddoctor" className='text-yellow-600 fund-bold text-xl'>add a doctors </Link></li>
+                                <li><Link to="/dashboard/managedoctor" className='text-yellow-600 fund-bold text-xl'>Managedoctor  </Link></li>
                             </>
                         }
 
